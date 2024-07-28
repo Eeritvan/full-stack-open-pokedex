9fd7b2b0faec258632b8ea07281ed6f24f8deaa0
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2024-07-28 15:02:35 +0300

    updated to app version 12

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static("dist"))
 
 app.get('/version', (_req, res) => {
-  res.send('11')
+  res.send('12')
 })
 
 app.get('/health', (_req, res) => {
